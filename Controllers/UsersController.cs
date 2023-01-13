711a47c43c6394f8a916bab9bd4565b4297bdc19
Dawson Coombs <dawsontech0@gmail.com>
2023-01-12 20:18:48 -0500

    Created Login and Registration for User model.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,120 @@
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+public class UsersController : Controller
+{
+    private int? uid
+    {
+        get
+        {
+            return HttpContext.Session.GetInt32("UUID");
+        }
+    }
+
+    private bool loggedIn
+    {
+        get
+        {
+            return uid != null;
+        }
+    }
+
+    private SocialMediaContext db;
+    public UsersController(SocialMediaContext context)
+    {
+        db = context;
+    }
+
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        if(loggedIn)
+        {
+            return RedirectToAction("All");
+        }
+        return View("Index");
+    }
+
+    [HttpPost("/register")]
+    public IActionResult Register(User newUser)
+    {
+        if(ModelState.IsValid)
+        {
+            if(db.Users.Any(u => u.Email == newUser.Email))
+            {
+                ModelState.AddModelError("Email", "is taken");
+            }
+        }
+
+        if (ModelState.IsValid == false)
+        {
+            return Index();
+        }
+
+        PasswordHasher<User> hashBrowns = new PasswordHasher<User>();
+        newUser.Password = hashBrowns.HashPassword(newUser, newUser.Password);
+
+        db.Users.Add(newUser);
+        db.SaveChanges();
+
+        HttpContext.Session.SetInt32("UUID", newUser.UserId);
+        // HttpContext.Session.SetString("Username", newUser.Username);
+        return RedirectToAction("All", "Posts");
+    }
+
+    [HttpPost("/login")]
+    public IActionResult Login(LoginUser loginUser)
+    {
+        if(ModelState.IsValid == false)
+        {
+            return Index();
+        }
+
+        User? dbUser = db.Users.FirstOrDefault(u => u.Email == loginUser.LoginEmail);
+
+        if (dbUser == null)
+        {
+            ModelState.AddModelError("LoginEmail", "not found");
+            return Index();
+        }
+
+        PasswordHasher<LoginUser> hashBrowns = new PasswordHasher<LoginUser>();
+        PasswordVerificationResult pwCompareResult = hashBrowns.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+        if (pwCompareResult == 0)
+        {
+            ModelState.AddModelError("LoginPassword", "not found");
+            return Index();
+        }
+
+        HttpContext.Session.SetInt32("UUID", dbUser.UserId);
+        // HttpContext.Session.SetString("Username", dbUser.Username);
+        return RedirectToAction("All", "Posts");
+    }
+
+    [HttpPost("/logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    // [HttpGet("/profile")]
+    // public IActionResult Profile()
+    // {
+    //     if(!loggedIn)
+    //     {
+    //         return RedirectToAction("Index");
+    //     }
+
+    //     User? dbUser = db.Users.Include(u => u.UserPosts).FirstOrDefault(u => u.UserId == uid);
+
+    //     if(dbUser == null)
+    //     {
+    //         return RedirectToAction("Index");
+    //     }
+    //     return View("Profile", dbUser);
+    // }
+}
---
+++ Models/User.cs
@@ -0,0 +1,41 @@
+#pragma warning disable CS8618
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SocialMedia.Models;
+
+public class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [Required(ErrorMessage = "is required!")]
+    [Display(Name = "Name")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "is required!")]
+    [Display(Name = "Email")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "is required!")]
+    [MinLength(8, ErrorMessage = "must be at least 8 characters!")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [NotMapped] // doesnt add colum to db
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "doesnt match password!")]
+    [Display(Name = "Confirm Password")]
+    public string ConfirmPassword { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    // foreign key
+
+    // List<Post> UserPosts { get; set; } = new List<Post>();
+
+    // public List<UserPostLike> UserLikes { get; set; } = new List<UserPostLike>();
+
+}
